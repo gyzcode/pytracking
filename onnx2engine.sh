5c005834c9d0fe9f6fbc0eed9113f87c234d45fe
Yuzhang Gu <2238758650@qq.com>
2020-08-17 07:53:06 +0800

    nx workspace

--- onnx2engine.sh
+++ onnx2engine.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-trtexec --onnx=pytracking/networks/dimp18_training_output.onnx --workspace=4096 --fp16 --saveEngine=pytracking/networks/dimp18_training_output.engine
+trtexec --onnx=pytracking/networks/dimp18_training_output.onnx --workspace=3072 --fp16 --saveEngine=pytracking/networks/dimp18_training_output.engine
 
-trtexec --onnx=pytracking/networks/dimp18_test_output.onnx --workspace=4096 --fp16 --saveEngine=pytracking/networks/dimp18_test_output.engine
+trtexec --onnx=pytracking/networks/dimp18_test_output.onnx --workspace=3072 --fp16 --saveEngine=pytracking/networks/dimp18_test_output.engine
